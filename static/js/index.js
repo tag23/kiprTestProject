eb9f66b27fd81b1735ad120e6392945dfb9cd6e2
tag23 <tag1998@mail.ru>
2019-08-14 22:12:56 +0300

    second commit

--- static/js/index.js
+++ static/js/index.js
@@ -10,7 +10,26 @@ $('.header-menu__locale-lang').click(function() {
     $(this)
         .addClass('header-menu__locale-lang--active')
         .siblings()
-        .removeClass('header-menu__locale-lang  --active');
+        .removeClass('header-menu__locale-lang--active');
+});
+$('.footer-menu__navigation-item').click(function() {
+    $(this)
+        .addClass('footer-menu__navigation-item--active')
+        .siblings()
+        .removeClass('footer-menu__navigation-item--active');
+});
+$(document).ready(function () {
+    $('.container-gallery__description-control-prev-icon').addClass('disabled');
 });
 
-$('.carousel').carousel();
+$('#first-slider').on('slid.bs.carousel', function (event) {
+    id = Array.prototype.indexOf.call(event.target.childNodes[1].children, event.target.querySelector('.active'));
+    if (id === 0) {
+        $('.container-gallery__description-control-prev-icon').addClass('disabled');
+    } else if (id === 4) {
+        $('.container-gallery__description-control-next-icon').addClass('disabled');
+    } else {
+        $('.container-gallery__description-control-prev-icon').removeClass('disabled');
+        $('.container-gallery__description-control-next-icon').removeClass('disabled');
+    }
+});
